14d7f0464c69080a385274d862d6b6e747a7f28e
38219002 <38219002@L829WD210019.edu.cbe.ab.ca>
2025-09-10 11:49:28 -0600

    great introducer progress

--- Humans/src/humans/Introducer.java
+++ Humans/src/humans/Introducer.java
@@ -3,6 +3,26 @@ package humans;
 public class Introducer {
 
     public String createPublicIntroduction(Human person) {
-        return "";
+        try {
+            String intro = String.format("I am pleased to introduce %s %s.", person.getFirstName(), person.getLastName());
+            String pronoun = "";
+            if (person.getGender() == Gender.MALE) {
+                pronoun = "He";
+            } else if (person.getGender() == Gender.FEMALE) {
+                pronoun = "She";
+            } else {
+                pronoun = "They";
+            }
+            if (person instanceof Adult) {
+                Adult adult = (Adult) person;
+                intro += String.format(" %s works at %s and their occupation is %s.", pronoun, adult.getPlaceOfWork(), adult.getOccupation());
+            } else if (person instanceof Youth) {
+                Youth youth = (Youth) person;
+                intro += String.format(" %s goes to %s and is in grade %d.", pronoun, youth.getSchoolName(), youth.getSchoolGrade());
+            }
+            return intro;
+        } catch (Exception e) {
+            return "This person has issues and can't be introduced.";
+        }
     }
 }
